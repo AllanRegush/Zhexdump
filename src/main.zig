2aecb39831378676286e78a9414c5cabc8a7d3d6
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-12-01 20:33:27 -0600

    Print Contents

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+
 const process = std.process;
 const fs = std.fs;
 const io = std.io;
@@ -8,24 +9,34 @@ pub fn main() anyerror!void {
     var arena_instance = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena_instance.deinit();
     const arena = &arena_instance.allocator;
-
+    const print = std.io.getStdOut().writer().print;
     const args = try process.argsAlloc(arena);
     if (args.len != 2) {
-        std.debug.print("Usage: zhexdump <file>\n", .{});
+        try print("Usage: zhexdump <file>\n", .{});
         return;
     }
-    for (args) | arg | {
-        std.debug.print("{s} ", .{arg});
-    }
-    std.debug.print("\n", .{});
-    var file = try std.fs.cwd().openFile(args[1], .{});
+
+    const file = try std.fs.cwd().openFile(args[1], .{});
     defer file.close();
     const contents = try file.reader().readAllAlloc(
         arena,
-        1024 * 4,
+        1024 * 1024 * 4,
     );
-    std.debug.print("File: {s}\n", .{args[1]});
-    for (contents) | c | {
-        std.debug.print("0x:{x} ", .{c});
+    const BYTES_PER_LINE = 16;
+    var address: usize = 0;
+    std.debug.print("[0x{x:0>8}] ", .{address});
+    for (contents) |c| {
+        if (address % BYTES_PER_LINE == 0 and address != 0) {
+            try print("\n", .{});
+            try print("[0x{x:0>8}] ", .{address});
+        }
+
+        switch (c) {
+            0x00 => try print(".  ", .{}),
+            0xFF => try print("## ", .{}),
+            else => try print("{x:0>2} ", .{c}),
+        }
+
+        address += 1;
     }
 }
